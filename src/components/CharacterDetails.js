3aa8c510299623162eb79216bea8bbad87cdc014
Ana Salamero <asalamerosanz@gmail.com>
2021-06-16 19:21:37 +0200

    improved styles

--- src/components/CharacterDetails.js
+++ src/components/CharacterDetails.js
@@ -3,21 +3,36 @@ import { Link } from 'react-router-dom';
 const CharacterDetails = (props) => {
   return (
     <div key={props.info.id}>
-      <Link to="/">{`< `}Go back</Link>
+      <Link className="characterdetaillink" to="/">
+        {`< `}Go back
+      </Link>
       <header>
-        <h2>{props.info.name}</h2>
+        <h2 className="characterdetail_title">{props.info.name}</h2>
       </header>
-      <section>
+      <section className="characterdetail_section">
         <img
           src={props.info.photo}
           alt={props.info.name}
           title={props.info.name}
+          className="characterdetail_section-img"
         />
-        <ul>
-          <li>Status: {props.info.status}</li>
-          <li>Species: {props.info.class}</li>
-          <li>Origin: {props.info.origin}</li>
-          <li>Episodes: {props.info.episodes}</li>
+        <ul className="characterdetail_section-list">
+          <li>
+            <span className="characterdetail_section-listele">Status</span>
+            {props.info.status}
+          </li>
+          <li>
+            <span className="characterdetail_section-listele">Species</span>
+            {props.info.class}
+          </li>
+          <li>
+            <span className="characterdetail_section-listele">Origin</span>
+            {props.info.origin}
+          </li>
+          <li>
+            <span className="characterdetail_section-listele">Episodes</span>
+            {props.info.episodes}
+          </li>
         </ul>
       </section>
     </div>
